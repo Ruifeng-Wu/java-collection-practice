5cfd2c7ccbc804621dbbc80e6a429daa2539d69a
Ruifeng-Wu <296078293@qq.com>
2019-03-19 18:24:55 +0800

    feat:implement Filter.getCommonElements()

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -1,7 +1,8 @@
 package com.thoughtworks.collection;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class Filter {
 
@@ -12,18 +13,18 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        return array.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        return array.stream().filter(x -> x % 2 == 0).collect(toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        return array.stream().filter(x -> x % 3 == 0).collect(Collectors.toList());
+        return array.stream().filter(x -> x % 3 == 0).collect(toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        return null;
+        return firstList.stream().filter(x -> secondList.contains(x)).collect(toList());
     }
 
     public List<Integer> getDifferentElements() {
-        return array.stream().distinct().collect(Collectors.toList());
+        return array.stream().distinct().collect(toList());
     }
 }

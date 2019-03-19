67c4ec047ddce0934b3ee5b4f6c99093625d9f85
Ruifeng-Wu <296078293@qq.com>
2019-03-19 18:21:15 +0800

    feat:implement Filter.filterMultipleOfThree()

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -18,7 +18,7 @@ public class Filter {
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        return array.stream().filter(x -> x % 3 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {

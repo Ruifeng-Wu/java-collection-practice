c2012a11437bfad2d9c4cdc2ffb3a5c3ff7f4525
Ruifeng-Wu <296078293@qq.com>
2019-03-19 20:44:38 +0800

    feat:implement Reduce.getIndexOfFirstEven()

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -34,7 +34,14 @@ public class Reduce {
     }
 
     public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
+        int firstEvenIndex = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i)%2==0){
+                firstEvenIndex=i;
+                break;
+            }
+        }
+        return firstEvenIndex;
     }
 
     public boolean isEqual(List<Integer> arrayList) {
